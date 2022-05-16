25e90e3e0c3389e393867553be29069fbed991b6
Mimanchi <1640247539@qq.com>
2022-05-16 15:08:23 +0800

    添加Nav样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="nav">
-      <router-link to="/money">
+      <router-link to="/money" class="item" active-class="selected">
         <Icon name='money'/>
         记账
       </router-link>
       |
-      <router-link to="/labels">
+      <router-link to="/labels" class="item" active-class="selected">
       <Icon name='label'/>
       标签
       </router-link>
       |
-      <router-link to="/statistics">
+      <router-link to="/statistics" class="item" active-class="selected">
       <Icon name='statistics'/>
       统计
       </router-link>
@@ -35,6 +35,25 @@
 
 <style lang="scss" scoped>
     .nav{
-        border: 1px solid red;
+        display: flex;
+        box-shadow: 0 0 3px rgba(0,0,0,0.25);
+        flex-direction: row;
+        font-size: 12px;
+        > .item{
+            padding: 2px 0;
+            width: 33.33333%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            color: #FC94B4;
+            .icon{
+                width: 32px;
+                height: 32px;
+            }
+        }
+        > .item.selected{
+            color: #F40463;
+        }
     }
 </style>
