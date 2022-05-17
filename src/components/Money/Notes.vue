e1bad470c3eac0bf4d7a31641c88406647bff8c0
Mimanchi <1640247539@qq.com>
2022-05-17 18:55:23 +0800

    模块化：把Money.vue分成四个子组件

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <label class="notes">
+            <span class="name">备注</span>
+            <input type="text" placeholder="在这里添加备注">
+        </label>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name:'Notes',
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "~@/assets/style/helper.scss";
+    .notes {
+        font-size: 14px;
+        color: $color-highlight;
+        display: block;
+        padding: 0 16px;
+        display: flex;
+        align-items: center;
+        .name {
+            padding-right: 16px;
+        }
+        input {
+            height: 64px;
+            flex-grow: 1;
+            background: transparent;
+            border: none;
+            color: #FC94B4;
+            &::placeholder {
+                color: #FC94B4;
+            }
+        }  
+    }
+</style>
---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="tags">
+        <div class="new">
+            <button>新增标签</button>
+        </div>
+        <ul class="current">
+            <li>衣</li>
+            <li>食</li>
+            <li>住</li>
+            <li>行</li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    .tags {
+        flex-grow: 1;
+        font-size: 14px;
+        padding: 16px;
+        background-color: #FFE9E9;
+        display: flex;
+        flex-direction: column-reverse;
+        > .current {
+            display: flex;
+            flex-wrap: wrap;
+            > li {
+                font-size: 16px;
+                background: #FCACA4;
+                $h: 24px;
+                height: $h;
+                line-height: $h;
+                border-radius: $h/2;
+                padding: 0 16px;
+                margin-right: 12px;
+                margin-top: 4px;
+            }
+        }
+        > .new {
+            padding-top: 16px;
+            button {
+                background: transparent;
+                border: none;
+                border-bottom: 1px solid #999;
+                color: #E35084;
+                padding: 0 3px;
+            }
+        }
+    }
+    .layout-content {
+        display: flex;
+        flex-direction: column-reverse;
+    }
+</style>
