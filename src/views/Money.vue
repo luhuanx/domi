6b1e28754cb4082ae99fe88578465e3351b212af
Mimanchi <1640247539@qq.com>
2022-05-18 18:24:50 +0800

    添加createAt字段

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,6 @@
 <template>
     <layout class-prefix="layout">
-        {{record}}     
+        {{recordList}}     
         <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
         <Types :value.sync="record.type"/>
         <Notes @update:value="onUpdateNotes"/>
@@ -21,6 +21,7 @@
         notes: string 
         type: string 
         amount: number
+        createAt?: Date
     }
 
     @Component ({
@@ -28,7 +29,7 @@
     })
     export default class Money extends Vue{
         tags = ['衣','食','住','行'];
-        recordList: Record[] = [];
+        recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
         record: Record = {tags: [],notes: '',type: '-',amount: 0};
 
         onUpdateTags(value: string[]){
@@ -41,10 +42,9 @@
         }
 
         saveRecord(){
-            const record2 = JSON.parse(JSON.stringify(this.record));
-            this.recordList.push(record2);
-            console.log(this.recordList);
-            
+            const record2: Record = JSON.parse(JSON.stringify(this.record));
+            record2.createAt = new Date();
+            this.recordList.push(record2);            
         }
 
         @Watch('recordList')
