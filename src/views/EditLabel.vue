20082e3a771630843335543a865b03c6afdd071e
Mimanchi <1640247539@qq.com>
2022-05-19 22:05:26 +0800

    改造notes.vue

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,12 +1,17 @@
 <template>
     <Layout>
-    编辑标签
+        <div>
+            <Icon name="left"/>
+            <span>编辑标签</span>
+        </div>
+        <Notes field-name="标签名" placeholder="请输入标签名"/>
     </Layout>
 </template>
 <script lang = 'ts'>
     import tagListModel from '@/models/tagListModel';
     import Vue from 'vue'
     import {Component} from 'vue-property-decorator'
+    import Notes from '@/components/Money/Notes.vue'
     
     @Component
     export default class EditLabel extends Vue {
