d4625c573226d46e960249e9d3009e6042b57da2
Mimanchi <1640247539@qq.com>
2022-05-19 17:59:26 +0800

    封装出一个model

---
+++ src/model.ts
@@ -0,0 +1,14 @@
+const localStorageName = 'recordList';
+const model = {
+    clone(data: RecordItem[] | RecordItem) {
+        return JSON.parse(JSON.stringify(data));
+    },
+    fetch() {
+        return JSON.parse(window.localStorage.getItem(localStorageName) || '[]') as RecordItem[];
+    },
+    sava(data: RecordItem[]) {
+        window.localStorage.setItem(localStorageName, JSON.stringify(data));
+    }
+};
+
+export default model;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,22 +15,19 @@
     import Tags from '@/components/Money/Tags.vue'
     import NumberPad from '@/components/Money/NumberPad.vue'
     import {Component, Watch} from 'vue-property-decorator'
+    import model from '@/model';
 
-    type Record = {
-        tags: string[]
-        notes: string 
-        type: string 
-        amount: number
-        createAt?: Date
-    }
+    const recordList = model.fetch();
 
     @Component ({
         components: { NumberPad, Types, Tags, Notes}
     })
     export default class Money extends Vue{
         tags = ['衣','食','住','行'];
-        recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
-        record: Record = {tags: [],notes: '',type: '-',amount: 0};
+        recordList: RecordItem[] = recordList;
+        record: RecordItem = {
+            tags: [],notes: '',type: '-',amount: 0
+        };
 
         onUpdateTags(value: string[]){
             this.record.tags = value;
@@ -42,14 +39,14 @@
         }
 
         saveRecord(){
-            const record2: Record = JSON.parse(JSON.stringify(this.record));
+            const record2: RecordItem = model.clone(this.record);
             record2.createAt = new Date();
             this.recordList.push(record2);            
         }
 
         @Watch('recordList')
         onRecordListChange(){
-            window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
+            model.sava(this.recordList);
         }
 
     }
