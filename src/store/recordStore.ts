f8ce9e7797e2353dad1767a131f6261ad5ef4e53
Mimanchi <1640247539@qq.com>
2022-05-22 15:21:42 +0800

    模块化store，删除了models

---
+++ src/store/recordStore.ts
@@ -0,0 +1,24 @@
+import clone from '@/lib/clone';
+
+const localStorageName = 'recordList';
+
+const recordStore = {
+    recordList: [] as RecordItem[],
+    fetchRecords() {
+        this.recordList = JSON.parse(window.localStorage.getItem(localStorageName) || '[]') as RecordItem[];
+        return this.recordList;
+    },
+    savaRecords() {
+        window.localStorage.setItem(localStorageName, JSON.stringify(this.recordList));
+    },
+    createRecord(record: RecordItem) {
+        const record2: RecordItem = clone(record);
+        record2.createAt = new Date();
+        this.recordList?.push(record2);
+        recordStore.savaRecords();
+    }
+};
+
+recordStore.fetchRecords();
+
+export default recordStore;
