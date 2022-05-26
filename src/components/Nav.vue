159c694126b73bf9ffbcd719354c53031a5a20fe
Mimanchi <1640247539@qq.com>
2022-05-26 18:42:53 +0800

    完成统计页面

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,57 +1,56 @@
 <template>
-    <nav>
-      <router-link to="/money" class="item" active-class="selected">
-        <Icon name='money'/>
-        记一笔
-      </router-link>
-      <router-link to="/labels" class="item" active-class="selected">
-      <Icon name='label'/>
+  <nav>
+    <router-link to="/money"
+                 class="item"
+                 active-class="selected">
+      <Icon name='money' />
+      记一笔
+    </router-link>
+    <router-link to="/labels"
+                 class="item"
+                 active-class="selected">
+      <Icon name='label' />
       标签
-      </router-link>
-      <router-link to="/statistics" class="item" active-class="selected">
-      <Icon name='statistics'/>
+    </router-link>
+    <router-link to="/statistics"
+                 class="item"
+                 active-class="selected">
+      <Icon name='statistics' />
       统计
-      </router-link>
-    </nav>
+    </router-link>
+  </nav>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue'
-    
-    let importAll=(requireContext:__WebpackModuleApi.RequireContext)=>requireContext.keys().forEach(requireContext);
-    try {
-        importAll(require.context('../assets/icons',true,/\.svg$/));
-    } catch (error) {
-        console.log(error)
-    }
+import Vue from 'vue'
 
-    export default Vue.extend({
-        name:'Nav',
-    })
+export default Vue.extend({
+  name: 'Nav',
+})
 </script>
 
 <style lang="scss" scoped>
-    @import "~@/assets/style/helper.scss";
-    nav{
-        @extend %outerShadow;
-        display: flex;
-        flex-direction: row;
-        font-size: 12px;
-        > .item{
-            padding: 2px 0;
-            width: 33.33333%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-            color: #FC94B4;
-            .icon{
-                width: 32px;
-                height: 32px;
-            }
-        }
-        > .item.selected{
-            color: $color-highlight;
-        }
+@import '~@/assets/style/helper.scss';
+nav {
+  @extend %outerShadow;
+  display: flex;
+  flex-direction: row;
+  font-size: 12px;
+  > .item {
+    padding: 2px 0;
+    width: 33.33333%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    color: #fc94b4;
+    .icon {
+      width: 32px;
+      height: 32px;
     }
+  }
+  > .item.selected {
+    color: $color-highlight;
+  }
+}
 </style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -4,13 +4,10 @@
     <Tabs class-prefix="type"
           :data-source="recordTypeList"
           :value.sync="type" />
-    <Tabs class-prefix="interval"
-          :data-source="intervalList"
-          :value.sync="interval" />
     <ol>
-      <li v-for="(group,index) in result"
+      <li v-for="(group,index) in groupedList"
           :key="index">
-        <h3 class="title">{{group.title}}</h3>
+        <h3 class="title">{{beautify(group.title)}} <span>￥{{group.total}}</span></h3>
         <ol>
           <li v-for="(item,index) in group.items"
               :key="index"
@@ -31,11 +28,29 @@ import { Component } from 'vue-property-decorator'
 import Tabs from '../components/Tabs.vue'
 import intervalList from '@/constants/intervalList'
 import recordTypeList from '@/constants/recordTypeList'
+import dayjs from 'dayjs'
+import clone from '@/lib/clone'
 
 @Component({
   components: { Tabs },
 })
 export default class Statistics extends Vue {
+  beautify(string: string) {
+    const day = dayjs(string)
+    const now = dayjs()
+    if (day.isSame(now, 'day')) {
+      return '今天'
+    } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
+      return '昨天'
+    } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
+      return '前天'
+    } else if (day.isSame(now, 'year')) {
+      return day.format('M月D日')
+    } else {
+      return day.format('YYYY年M月D日')
+    }
+  }
+
   // eslint-disable-next-line no-undef
   tagString(tags: Tag[]) {
     return tags.length === 0 ? '无' : tags.join(',')
@@ -46,17 +61,40 @@ export default class Statistics extends Vue {
     return (this.$store.state as RootState).recordList
   }
 
-  get result() {
+  get groupedList() {
     const { recordList } = this
-    type HashTableValue = { title: string; items: RecordItem[] }
-
-    const hashTable: { [key: string]: HashTableValue } = {}
-    for (let i = 0; i < recordList.length; i++) {
-      const [date, time] = recordList[i].createAt!.split('T')
-      hashTable[date] = hashTable[date] || { title: date, items: [] }
-      hashTable[date].items.push(recordList[i])
+    if (recordList.length === 0) {
+      return []
     }
-    return hashTable
+
+    const newList = clone(recordList)
+      .filter((r) => r.type === this.type)
+      .sort((a, b) => dayjs(b.createAt).valueOf() - dayjs(a.createAt).valueOf())
+    type Result = { title: string; total?: number; items: RecordItem[] }[]
+    const result: Result = [
+      {
+        title: dayjs(newList[0].createAt).format('YYYY-MM-DD'),
+        items: [newList[0]],
+      },
+    ]
+    for (let i = 0; i < newList.length; i++) {
+      const current = newList[i]
+      const last = result[result.length - 1]
+      if (dayjs(last.title).isSame(dayjs(current.createAt), 'day')) {
+        last.items.push(current)
+      } else {
+        result.push({
+          title: dayjs(current.createAt).format('YYYY-MM-DD'),
+          items: [current],
+        })
+      }
+    }
+    result.map((group) => {
+      group.total = group.items.reduce((sum, item) => {
+        return sum + item.amount
+      }, 0)
+    })
+    return result
   }
 
   beforeCreate() {
